a0a10c324ad662da5c6a542b45aeb87006696e4d
admin <admin@YD4-21>
2022-07-21 17:43:27 +0900

    2022-07-21

---
+++ helloWorld/src/main/java/com/edu/EmpListServlet.java
@@ -0,0 +1,33 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/emplist.do")
+public class EmpListServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		EmpDAO eDAO = new EmpDAO();
+		List<Employee> list = eDAO.empList();
+		PrintWriter out = resp.getWriter();
+		out.print("<table border='1'>");
+		out.print("<thead><tr><th>사원번호</th><th>이름</th><th>이메일</th><th>입사일자</th><th>급여</th><th>직무</th></tr></thead>");
+		out.print("<tbody>");
+		for (Employee emp : list) {
+			out.print("<tr><td>" + emp.getEmployeeId() + "</td>" + "<td>" + emp.getLastName() + emp.getFirstName()
+					+ "</td>" + "<td>" + emp.getEmail() + "</td>" + "<td>" + emp.getHiredate() + "</td>" + "<td>"
+					+ emp.getSalary() + "</td>" + "<td>" + emp.getJogId() + "</td>");
+
+		}
+		out.print("</tbody>");
+		out.print("</table>");
+	}
+}
---
+++ helloWorld/src/main/java/com/edu/Employee.java
@@ -0,0 +1,57 @@
+package com.edu;
+
+import java.sql.Date;
+
+public class Employee {
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int salary;
+	private Date hiredate;
+	private String jogId;
+	
+	
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date date) {
+		this.hiredate = date;
+	}
+	public String getJogId() {
+		return jogId;
+	}
+	public void setJogId(String jogId) {
+		this.jogId = jogId;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
